eef1ec96cd1cd469ee1cdcae02ac8bb7d5dd9516
KASSithujaya <it24100722@my.sliit.lk>
2025-07-31 09:10:35 +0530

    Welcome is added

---
+++ Welcome.java
@@ -0,0 +1,16 @@
+Import java.util.Scanner;
+public class Welcome{
+    public static void main(string[] args){
+        Scanner name = new Scanner(System.in);
+
+        System.out.print("Enter your First Name: ");
+        String firstName = name.nextLine();
+
+        System.out.print("Enter your Last Name: ");
+        String lastName = name.nextLine();
+
+        System.out.println("Welcome to the Second Year" + firstName + " " + lastName +);
+       
+        name.close();
+    }
+}
